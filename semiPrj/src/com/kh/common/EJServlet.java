185c6f94c205e06b099ffa80e4aa7292f2b2d996
Eunjin <chocoding0@naver.com>
2022-08-03 17:10:55 +0900

    인코딩 변경

--- semiPrj/src/com/kh/common/EJServlet.java
+++ semiPrj/src/com/kh/common/EJServlet.java
@@ -14,7 +14,7 @@ public class EJServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		System.out.println("ÀÛµ¿µÊ..");
+		System.out.println("ì‘ë™ë¨..!");
 	
 	}
 }
