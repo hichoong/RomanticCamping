ce8941c50b0455a68d68172fa3e7d2550e0ce951
Eunjin <chocoding0@naver.com>
2022-08-20 19:39:11 +0900

    사용자 예약 확인 페이지

--- semiPrj/src/com/kh/member/controller/MyReservationPageController.java
+++ semiPrj/src/com/kh/member/controller/MyReservationPageController.java
@@ -13,7 +13,7 @@ public class MyReservationPageController extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/views/member/myReservationPage.jsp").forward(req, resp);
+		resp.sendRedirect(req.getContextPath()+"/member/reservationCheck?type=S");
 	}
 	
 }
