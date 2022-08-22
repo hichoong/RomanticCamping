b18e28a0dbe7e0f49f111ea1bbee4f6749dc0100
DESKTOP-NENJELC\pc <jysj8212@naver.com>
2022-08-22 22:01:16 +0900

    buisness_camp(0822)_sjy
    등록,조회 서비스 dao  수정

--- semiPrj/src/com/sjy/buisness/camp/service/BsCampService.java
+++ semiPrj/src/com/sjy/buisness/camp/service/BsCampService.java
@@ -8,10 +8,12 @@ import java.util.UUID;
 import static com.kh.common.JDBCTemplate.*;
 
 import com.kh.camplist.hashtag.vo.HashTagVo;
+import com.kh.camplist.theme.vo.ThemeVo;
 import com.sjy.buisness.camp.repository.BsCampDao;
 import com.sjy.buisness.camp.vo.BsCampVo;
 import com.sjy.buisness.camp.vo.BsCampZoneVo;
 import com.sjy.buisness.camp.vo.CampAttachmentVo;
+import com.sjy.buisness.hashmapping.vo.HashMappingVo;
 
 public class BsCampService {
 	BsCampDao dao = new BsCampDao();
@@ -39,6 +41,16 @@ public class BsCampService {
 		List<BsCampVo> bsVo = dao.selectAllCamp(conn, no); 
 		return bsVo;
 	}
+	/*
+	 * 선택한 캠핑장 정보 가져오기
+	 */
+	public BsCampVo selectOneCamp(String no, String campCode) {
+		Connection conn = null;
+		conn = getConnection();
+		
+		BsCampVo bsVo = dao.selectOneCamp(conn, no, campCode); 
+		return bsVo;
+	}
 	
 	/*
 	 * 캠핑장 등록
@@ -56,33 +68,38 @@ public class BsCampService {
 		//캠핑장 메인 이미지
 		
 		//캠핑장 테이블
-		int result =0;
-		result = new BsCampDao().campInsert(conn, campVo, ahVo);
+		int result1 =0;
+		int result2 =0;
+		result1 = new BsCampDao().campInsert(conn, campVo, ahVo);
+		result2 = new BsCampDao().insertMainAh(conn, ahVo);
+				
 		//결과에따른 커밋 롤백
-		if (result==1) {
+		if (result1*result2==1) {
 			commit(conn);
 		}else {rollback(conn);}
 		
-		return result;
+		return result1*result2;
 	}
 
 	
 	/*
 	 * 캠핑장 구역 등록
 	 */
-	public int campZoneInsert(BsCampZoneVo[] zoneVo, int i) {
+	public int campZoneInsert(BsCampZoneVo[] zoneVo, int i, CampAttachmentVo ahVo2) {
 		Connection conn =null;
 		conn = getConnection();
 		
-		int result = 0;
-		result = new BsCampDao().campZoneInsert(conn, zoneVo, i);
+		int result1 = 0;
+		int result2 = 0;
+		result1 = new BsCampDao().campZoneInsert(conn, zoneVo, i);
+		result2 = new BsCampDao().ZoneImgInsert(conn, ahVo2,i);
 		//결과에따른 커밋 롤백
-		if (result==1) {
+		if (result1*result2==1) {
 			commit(conn);
 		}else {rollback(conn);}
 		
 		
-		return result;
+		return result1*result2;
 	}
 
 	/*
@@ -118,8 +135,41 @@ public class BsCampService {
 		conn =  getConnection();
 		
 		int result = dao.insertHash(conn, hashVo); 
-		
+		if (result == 1) {
+			commit(conn);
+		}else {
+			rollback(conn);
+		}
 		return result;
+	}
+	
+	/*선택한 캠핑장에서 테마, 해쉬태그 , 시설현황 가져오기*/
+	public BsCampVo selectOneTheme(String campCode) {
+		Connection conn = getConnection();
+		BsCampVo vo = dao.selectOneTheme(conn,campCode);
+		close(conn);
+		return vo;
+	}
+	public List<HashMappingVo> selecOnetHashTag(String campCode) {
+		Connection conn = getConnection();
+		List<HashMappingVo> list = dao.selectOneHashTag(conn,campCode);
+		close(conn);
+		return list;
+	}
+	
+//	public List<HashTagVo> selecOnetF(String campCode) {
+//		Connection conn = getConnection();
+//		List<HashTagVo> list = dao.selectOneHashTag(conn,campCode);
+//		close(conn);
+//		return list;
+//	}
+
+	public List<BsCampZoneVo> selectZoneAll(String campCode) {
+		Connection conn = null;
+		conn = getConnection();
+		List<BsCampZoneVo> list = dao.selectZoneAll(conn, campCode);
+		System.out.println("sev zli"+list);
+		return list;
 	}
 
 
